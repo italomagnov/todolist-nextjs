611f43df86b9c44309b4a44fdc6785ad87afdda5
italomagnov <italomagnov@gmail.com>
2022-12-07 02:11:29 -0400

    criação do layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,37 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
+    return (
+        <div className="w-screen h-screen flex flex-col items-center bg-zinc-900">
+            <div className="max-w-lg">
+                <h1 className="font-base font-extrabold text-5xl text-center pt-16 text-white">
+                    ToDoList App - NextJs
+                </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+                {/* Componente Input */}
+                <div className="w-full px-4 py-8 flex items-center justify-center gap-4">
+                    <input
+                        className="w-full px-4 py-2 border-2 rounded-md border-violet-500 outline-none bg-zinc-900 text-white"
+                        type="text"
+                        placeholder="Insira a task"
+                    />
+                    <button className="px-3 py-2 rounded-lg shadow-md bg-violet-500 text-white -rotate-12">
+                        ✏️
+                    </button>
+                </div>
 
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+                {/* Componente lista com as Tasks */}
+                <div className="w-full pt-8 px-3">
+                    <ul className="flex flex-col items-center justify-between">
+                        <div className="bg-task bg-cover w-full h-16 mb-4 flex items-center justify-around">
+                            <li className="font-semibold text-xl">
+                                Comprar açaí com laele
+                            </li>
+                            <span className="font-semibold text-xl cursor-pointer">
+                                X
+                            </span>
+                        </div>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+    );
 }
